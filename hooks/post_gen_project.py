48489e4089156fa657d0002926e70c232b213249
App Generator <support@appseed.us>
2021-09-03 10:56:31 +0300

    Update PRE/POST Hooks with code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,12 +4,42 @@ import logging
 import os
 import shutil
 import sys
+from distutils.dir_util import copy_tree
 
 _logger = logging.getLogger()
+TMP_DIR = './tmp/' 
 
 def post_hook():
     """Flag the POST gen HOOK execution"""
     _logger.warning("POST Gen hook executed")
+
+    try:
+
+        print(' *** Copy assets') 
+        fromDirectory = TMP_DIR + "core/static"
+        toDirectory   = "./core/static"
+
+        copy_tree(fromDirectory, toDirectory)
+        print(' ...done')
+
+        print(' *** Copy templates') 
+        fromDirectory = TMP_DIR + "core/templates"
+        toDirectory   = "./core/templates"
+
+        copy_tree(fromDirectory, toDirectory)
+        print(' ...done')
+
+        shutil.copy( TMP_DIR + "CHANGELOG.md", "CHANGELOG.md")
+        shutil.copy( TMP_DIR + "LICENSE.md"  , "LICENSE.md")
+
+        print('Remove `tmp` dir')
+        shutil.rmtree( TMP_DIR )
+        print(' ...done')
+
+    except OSError as e:
+        #_logger.warning("While attempting to remove file(s) an error occurred")
+        _logger.warning(f"Error: {e}")
+        sys.exit(0)
 
     # Exit with success     
     sys.exit(0)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,14 +4,37 @@ import logging
 import os
 import shutil
 import sys
+import git 
 
 _logger = logging.getLogger()
+TMP_DIR = './tmp/' 
 
 def pre_hook():
     """Flag the PRE gen HOOK execution"""
     _logger.warning("PRE Gen hook executed")
 
-    # Exit with success     
+    try:
+
+        # Default value = Volt
+        theme = "{{cookiecutter.theme}}"
+        repo  = 'https://github.com/app-generator/django-dashboard-volt.git'
+
+        if 'soft-ui' == theme:
+            repo  = 'https://github.com/app-generator/django-soft-ui-dashboard.git'
+
+        if 'datta-able' == theme:
+            repo  = 'https://github.com/app-generator/django-datta-able.git'
+
+        print(' Cloning theme: <' + theme + '>, REPO: ' + repo )
+
+        repo = git.Repo.clone_from( repo, TMP_DIR, branch='master')
+        print(' ...done')
+               
+    except OSError as e:
+        _logger.warning(f"Error: {e}")
+        sys.exit(0)
+
+    # Exit with success
     sys.exit(0)
 
 if __name__ == "__main__":
